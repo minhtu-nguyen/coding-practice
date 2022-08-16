dcad48e4188724b717c29c172f431dc553e37b51
minhtu-nguyen <minhtu-nguyen@outlook.com>
2022-08-17 01:35:37 +0700

    Complete solution for Contains Duplicate problem

---
+++ neet-code/contains_duplicate.py
@@ -0,0 +1,15 @@
+class Solution:
+  def containsDuplicaet(self, nums: List[int]) --> bool:
+    hashset = set()
+
+    for n in nums:
+      if n in hashset:
+        return True
+      hashset.add(n)
+    return False
+
+"""
+Brute force: 2 loops - O(n^2) and O(1)
+Sort first: O(nlogn) and O(1)
+Extra mem: hashset - O(n) and O(n)
+"""
