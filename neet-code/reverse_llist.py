7c222de31c881624742790210c656729694787b5
minhtu-nguyen <minhtu-nguyen@outlook.com>
2022-08-30 23:28:45 +0700

    Complete solution for Reverse Link List problem

---
+++ neet-code/reverse_llist.py
@@ -0,0 +1,31 @@
+class ListNode:
+  def __init__(self, x):
+    self.val = x
+    self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+      prev, curr = None, head
+
+      while curr:
+        temp = curr.next
+        curr.next = prev
+        prev = curr
+        curr = temp
+      return prev
+"""
+Iterative: 2 pointers curr and prev (null) O(n) O(1)
+Recursive: O(n) O(n)
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+      if not head:
+        return None
+
+      newHead = head
+      if head.next:
+        newHead = self.reverseList(head.next)
+        head.next.next = head
+      head.next = None
+
+      return newHead
+"""
