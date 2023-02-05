7c404d6675505f0593ea5dbd3efb803807282070
minhtu-nguyen <minhtu-nguyen@outlook.com>
2023-02-05 18:54:29 +0700

    Add Non-overlapping Intervals

---
+++ neet-code/nonoverlapping_intervals.py
@@ -0,0 +1,17 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort()
+        res = 0
+        prevEnd = intervals[0][1]
+        for start, end in intervals[1:]:
+            if start >= prevEnd:
+                prevEnd = end
+            else:
+                res += 1
+                prevEnd = min(end, prevEnd)
+        return res
+
+"""
+Brute force: O(2^n) 2 choices for each interval
+Better: sort O(nlogn), keep minEnd
+"""
