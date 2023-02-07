20d89ee90f9a8a31e89c5ec72b0167824653dd6a
minhtu-nguyen <minhtu-nguyen@outlook.com>
2023-02-07 15:48:06 +0700

    Add Missing Number

---
+++ neet-code/missing_numbers.py
@@ -0,0 +1,13 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        res = len(nums)
+
+        for i in range(len(nums)):
+            res += i - nums[i]
+        return res
+
+"""
+HashSet O(n)
+XOR ???
+Sum - Sum
+"""
