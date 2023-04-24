fc8c998e9a63d0d3d44b84fa7be6f4ee2ebf588c
minhtu-nguyen <minhtu-nguyen@outlook.com>
2023-04-24 20:34:24 +0700

    Add 3 Sum

--- educative/Patterns.py
+++ educative/Patterns.py
@@ -2,7 +2,7 @@
 ##Valid Palindrome
 '''
 Write a function that takes a string s as input and checks whether it’s a palindrome or not.
-Understanding: 
+Flow: 
 - Initialize two pointers and traverse them towards the middle of the string.
 - Check if the current pair of elements is identical or not.
 - If they are not identical, return False, else, move both pointers by one index toward the middle.
@@ -23,3 +23,54 @@ def is_palindrome(s):
         left = left + 1 
         right = right - 1
     return True
+
+##Sum of Three Values
+'''
+Given an array of integers, nums, and an integer value, target, determine if there are any three integers in nums whose sum is equal to the target, that is, nums[i] + nums[j] + nums[k] == target. Return TRUE if three such integers exist in the array. Otherwise, return FALSE.
+Flow: 
+- Sort the input array in ascending order.
+- Iterate over the entire sorted array to find the triplet whose sum is equal to the target.
+- In each iteration, make a triplet by storing the current array element and the other two elements using two pointers (low and high), and calculate their sum.
+-Adjust the calculated sum value, until it becomes equal to the target value, by conditionally moving the pointers, low and high.
+- Return TRUE if the required sum is found. Otherwise, return FALSE.
+Naive approach: The naive approach to solving this problem is to use three nested loops. Each nested loop starts at the index greater than its parent loop. O(n^3) - O(1).
+Optimized approach: First, sort the array in ascending order. To find a triplet whose sum is equal to the target value, loop through the entire array. In each iteration:
+Store the current array element and set up two pointers (low and high) to find the other two elements that complete the required triplet.
+The low pointer is set to the current loop’s index + 1.
+The high is set to the last index of the array.
+Calculate the sum of array elements pointed to by the current loop’s index and the low and high pointers.
+If the sum is equal to target, return TRUE.
+If the sum is less than target, move the low pointer forward.
+If the sum is greater than target, move the high pointer backward.
+Repeat until the loop has processed the entire array. If, after processing the entire array, we don’t find any triplet that matches our requirement, we return FALSE.
+Sort = O(nlog(n)) | nested loop = O(n^2) = O(nlog(n) + n^2) = O(n^2) || built-in sort space: O(n)
+  nums_lists = [[3, 7, 1, 2, 8, 4, 5],
+                [-1, 2, 1, -4, 5, -3],
+                [2, 3, 4, 1, 7, 9],
+                [1, -1, 0],
+                [2, 4, 2, 7, 6, 3, 1]]
+
+  targets = [10, 7, 20, -1, 8]
+'''
+def find_sum_of_three(nums, target):
+    # Sort the input list
+    nums.sort()
+    # Fix one integer at a time and find the other two
+    for i in range(0, len(nums)-2): #distinct: i can’t be equal to low or high, and low can’t be equal to high
+        # Initialize the two pointers
+        low = i + 1
+        high = len(nums) - 1
+        # Traverse the list to find the triplet whose sum equals the target
+        while low < high:
+            triplet = nums[i] + nums[low] + nums[high]
+            # The sum of the triplet equals the target
+            if triplet == target:
+                return True                
+            # The sum of the triplet is less than target, so move the low pointer forward
+            elif triplet < target:
+                low += 1            
+            # The sum of the triplet is greater than target, so move the high pointer backward
+            else:
+                high -= 1    
+    # No such triplet found whose sum equals the given target
+    return False
