ef9026f7915489a50d56a9a468e0e3ada55db1dc
minhtu-nguyen <minhtu-nguyen@outlook.com>
2023-05-04 09:49:18 +0700

    Search Suggestions System

--- educative/Patterns.py
+++ educative/Patterns.py
@@ -4474,6 +4474,51 @@ class Trie():
             node = node.children.get(c)
         return True
 
+## Search Suggestions System
+'''
+Given an array of strings called products and a word to search, design a system that, when each character of the searched word is typed, suggests at most three product names from products. Suggested products should share a common prefix with the searched word. If more than three products exist with a common prefix, return the three product names that appear first in lexicographical order.
+Return the suggested products, which will be a list of lists after each character of searched word is typed.
+Flow:
+- Insert all product names in a trie, creating a node for each new character in a word.
+- As each new letter of the searched word is received, retrieve all words in the trie whose initial characters match. For example, if “gam” has been typed in, it may match “game”, “games”, “gamify” and “gamma”.
+- If there are more than three matched strings, return the ones that appear first in the lexicographic order. In our example, we would return “game”, “games” and “gamify”.
+Naive approach: A naive approach would be to first sort the given product data in lexicographic order to help with retrieving three matching products. Next we’ll make a substring for every character of the word to be searched. We’ll search in the list of product names to check whether the current substring exists or not. If it exists, we’ll store the results (containing matching product names) for the current substring. We’ll repeat this process until we have traversed the whole list of product names. O(m*n + nlogn) - O(1)
+Optimized approach: I O(w) - S O(m)
+'''
+class Trie(object):
+    def __init__(self):
+        self.root = TrieNode()
+
+
+    def insert(self, data):
+        node = self.root
+        idx = 0
+        for char in data:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+            if len(node.search_words) < 3:
+                node.search_words.append(data)
+            idx += 1
+
+    def search(self, word):
+        result, node = [], self.root
+        for i, char in enumerate(word):
+            if char not in node.children:
+                temp = [[] for _ in range(len(word) - i)]
+                return result + temp
+            else:
+                node = node.children[char]
+                result.append(node.search_words[:])
+        return result
+
+
+def suggested_products(products, search_word):
+    products.sort()
+    trie = Trie()
+    for x in products:
+        trie.insert(x)
+    return trie.search(search_word)
 
 
 
