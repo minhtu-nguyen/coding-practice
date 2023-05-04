efc9902df594e4ede25e35b6fecb996767d0accc
minhtu-nguyen <minhtu-nguyen@outlook.com>
2023-05-04 20:47:41 +0700

    Design Tic-Tac-Toe

--- educative/Patterns.py
+++ educative/Patterns.py
@@ -5039,6 +5039,87 @@ def permute_palindrome(st):
     else:
         return False
 
+## Design Tic-Tac-Toe
+'''
+Suppose that two players are playing a tic-tac-toe game on an n×n board. They’re following specific rules to play and win the game.
+- A move is guaranteed to be valid if a mark is placed on an empty block.
+- No more moves are allowed once a winning condition is reached.
+- A player who succeeds in placing n of their marks in a horizontal, vertical, or diagonal row wins the game.
+Your task is to implement a TicTacToe class, which will be used by two players to play the game and win fairly.
+Keep in mind the following functionalities that need to be implemented:
+- The TicTacToe class, which declares an object to create the board.
+- Init (n), which initializes the object of TicTacToe to create the board of size n.
+- Move (row, col, player) indicates that the player with ID player plays at the board’s cell (row,col). The move is guaranteed to be a valid move. At each move, this function returns the player ID if any player wins and returns 0 if no one wins.
+Flow:
+- Initialize arrays, rows, and cols of size n with 0 at each index for each player.
+- If rows[i] or cols[j] is equal to n, the player wins.
+- Initialize integer variables, diagonal and anti-diagonal, for each player.
+- Increment the diagonal and anti-diagonal integers whenever a player marks a cell on each of the diagonal.
+- If the diagonal or anti-diagonal is equal to n, the player wins.
+Naive approach: check on every move if the current player won the game. Each player marks the cell at row and col to make a move on the board. One player can win the game using the following four ways:
+- Player marked the complete row.
+- Player marked the complete column.
+- Player marked all the diagonal cells, starting from the top left corner to the bottom right corner of the board.
+- Player marked all the anti-diagonal cells, starting from the top right corner to the bottom left corner of the board.
+We mark the row and col on the board for the current with the player’s ID on every move. Then, we check the conditions to check if the current player wins.
+- Check the condition for the row by iterating over all the columns but keeping the row index constant.
+- Check the condition for the col by iterating over all the rows but keeping the column index constant.
+- Check the condition for diagonal by iterating the board and keeping the same row and column index values.
+- Check the condition for anti-diagonal by iterating the board and keeping the column equal to n−row−1.
+O(n) - O(n^2)
+Optimized approach: O(n) - O(n)
+'''
+class TicTacToe:
+    # TicTacToe class contains rows, cols, diagonal,
+    # and anti_diagonal to create a board.
+    # Constructor is used to create n * n tic - tac - toe board.
+    def __init__(self, n):
+        self.rows = [0] * (n)
+        self.cols = [0] * (n)
+        self.diagonal = 0
+        self.anti_diagonal = 0
+        self.board = []
+        for i in range(n):
+            st = ""
+            for j in range(n):
+                st += "-"
+            self.board.append(st)
+
+    # Move function will allow the players to play the game
+    # for given row and col.
+    def move(self, row, col, player):
+        current_player = -1
+        if player == 1:
+            current_player = 1
+
+        self.rows[row] += current_player
+        self.cols[col] += current_player
+
+        if row == col:
+            self.diagonal += current_player
+
+        if col == (len(self.cols) - row - 1):
+            self.anti_diagonal += current_player
+
+        st = list(self.board[row])
+        st[col] = str(current_player)
+        
+        st = list(self.board[row])
+        if current_player == 1:
+            st[col] = "O"
+        else:
+            st[col] = "X"
+        self.board[row] = "".join(st)
+
+        n = len(self.rows)
+
+        if abs(self.rows[row]) == n or abs(self.cols[col]) == n or abs(self.diagonal) == n or abs(self.anti_diagonal) == n:
+            return player
+        return 0
+
+
+
+
 
 ### *** Practice
 ## 2 pointers - Valid Palindrome II
