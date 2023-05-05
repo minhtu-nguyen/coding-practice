444d8e18b5d5e9736972b66d12a7af7fae4c0f19
minhtu-nguyen <minhtu-nguyen@outlook.com>
2023-05-05 09:45:18 +0700

    Min Stack

--- educative/Patterns.py
+++ educative/Patterns.py
@@ -5883,6 +5883,73 @@ class RandomSet():
         """
         return choice(self.store)
 
+## Min Stack
+'''
+Design a custom stack, Min Stack. Implement the Min Number(), Push() and Pop() methods:
+- Pop(): Removes and returns from the stack the value that was most recently pushed on to it.
+- Push(): Pushes the provided value on to the stack.
+- Min Number(): Returns the minimum value in the stack in O(1) time.
+Flow:
+- We use two stacks, main and min, to store all incoming elements.
+- Push the provided value on to the main stack.
+- Compare the provided value with the element on top of the min stack. If it’s smaller, then push it on to the min stack. Otherwise, make a copy of the top value of the min stack and push it on to the min stack again.
+O(1) - O(n)
+'''
+class MainStack:
+    def __init__(self):
+        self.stack_list = []
+
+    def is_empty(self):
+        return len(self.stack_list) == 0
+
+    def top(self):
+        if self.is_empty():
+            return None
+        return self.stack_list[-1]
+
+    def size(self):
+        return len(self.stack_list)
+
+    def push(self, value):
+        self.stack_list.append(value)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        return self.stack_list.pop()
+
+class MinStack:
+    # Initializing min and main stack
+    def __init__(self):
+        self.min_stack = MainStack()
+        self.main_stack = MainStack()
+
+    # Pop() removes and returns value from min_stack
+    def pop(self):
+        self.min_stack.pop()
+        # Returns the popped value from main_stack
+        return self.main_stack.pop()
+
+    # Pushes values into min_stack
+    def push(self, value):
+        self.main_stack.push(value)
+
+        # If the min_stack is empty, or the value being pushed is less than
+        # the minimum (top) value of min_stack
+        if self.min_stack.is_empty() or value < self.min_stack.top():
+            # Push this new value to the min_stack
+            self.min_stack.push(value)
+        else:
+            # Keep the minimum value at the top of min_stack
+            self.min_stack.push(self.min_stack.top())
+
+    # Returns minimum value from min_stack in O(1) Time
+    def min_number(self):
+        if self.min_stack.is_empty():
+            return None
+        else:
+            return self.min_stack.top()
+
 
 ### *** Practice
 ## 2 pointers - Valid Palindrome II
