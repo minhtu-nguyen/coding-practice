f81ad8c1db577d0a811ad943427a29a47354c856
minhtu-nguyen <minhtu-nguyen@outlook.com>
2023-05-07 01:00:09 +0700

    Count of Subset Sum

--- educative/DP.py
+++ educative/DP.py
@@ -260,3 +260,116 @@ def subset_sum(arr, total): # main function
                 dp[i][j] = dp[i-1][j] or dp[i-1][j-arr[i-1]]
     
     return dp[n][total]
+
+## Count of Subset Sum
+'''
+Given a set of positive numbers nums and a value target_sum, count the total number of subsets of the given set whose sum is equal to the target_sum.
+Naive approach: we will divide our problem into smaller subproblems, starting from the start of the nums list and for each element, we will do the following steps:
+Check for the following base cases:
+- If the target_sum is 0, it means that we can reach the sum of 0 without including any element in our subset resulting in an empty subset, so we return 1 to include an empty subset.
+- If we have traversed all of our elements then we cannot proceed further to calculate the required sum. So we return 0.
+Consider the current element for the required sum.
+- If it contributes to making up the target_sum, we subtract the target_sum from the current number and proceed on to the next number with the new target sum of target_sum – nums[current_index].
+- Do not consider the contribution of the current element for the target_sum and move on to consider the rest of the numbers.
+O(2^n) - O(n)
+---
+Optimized solution
+Top-down solution: The 2-D array will be of size input set size×(target+1) and initialized with −1 to indicate that these subproblems are yet to be solved. The rows correspond to each element in the input array and the columns correspond to the remaining sums from 0 to the target sum. So, for a given number and the remaining sum, we’ll do the following in the helper function:
+Check the base cases as done in the naive approach.
+If you look at the code shown below, you will see that we are checking if the count of subsets for a given element and the remaining sum has already been evaluated or not.
+- If it hasn’t already been evaluated, evaluate it as done in the naive approach and store the result in the 2-D array at lookup_table[current_index][required_sum]. The current_index specifies the index of the current element in the nums array.
+- If it has already been evaluated, fetch the computed result from lookup_table[current_index][required_sum].
+O(n * s) - O(n * s)
+Bottom-up solution: A 2-D array of size s×(t+1), where s is the input set size and t is the target sum, which is used to store the results of subproblems. All the cells in the table are initialized to 0. In each iteration, we consider a number from the input set and check if that number can be included in our subset to reach the remaining sum. If you look at the code shown below, you will see the following steps to calculate the count of the subset sum:
+Check the base case for the first number:
+- If the first number in the input array is 0, we update the cell at lookup[0][0] to 2 because the empty set and the set 0 will both contribute to the zero sum.
+- If the first number is not 0, we update the cell at lookup[0][0] to 1 because only the empty set will contribute to the zero sum. Also, if the number is less than the target, it’ll contribute towards the target, so update the cell at lookup[0][nums[0]] to 1.
+We iterate the input array starting from the 1st index, and for each of the numbers, we perform two steps for each required_sum from 0 to target:
+- Exclude the number. Get the count of all the subsets before the given number by fetching the value from lookup[current-1][required_sum].
+- Include the number if its value is not more than the required_sum. Get the count of all the subsets for the remaining target of required_sum - nums[current] by fetching it from lookup[current-1][required_sum - nums[current]].
+- Add the counts and store it in lookup[current][required_sum].
+After all the calculations, lookup[nums_len - 1][target_sum] has the count of all the subsets having the sum equal to the target_sum.
+O(n * s) - O(n * s)
+'''
+# Naive 
+# main function
+def count_subset_sum(nums, target_sum):
+    return count_subset_sum_rec(nums, target_sum, 0)
+
+# helper function
+def count_subset_sum_rec(nums, target_sum, current_index):
+    # base cases
+    if target_sum == 0:
+        return 1
+    if current_index >= len(nums):
+        return 0
+
+    # if the number at current_index does not exceed the 
+    # target_sum, we include it
+    sum1 = 0
+    if nums[current_index] <= target_sum:
+        sum1 = count_subset_sum_rec(nums, target_sum - nums[current_index], current_index + 1)
+
+    # recursive call, excluding the number at the current_index
+    sum2 = count_subset_sum_rec(nums, target_sum, current_index + 1)
+
+    return sum1 + sum2
+
+# Optimized
+# -- Top down
+def count_subset_sum(nums, target_sum):
+    # lookup table of size len(nums) x (targetSum + 1)
+    lookup_table = [[-1 for i in range(target_sum + 1)] for j in range(len(nums))]
+    return count_subset_sum_rec(nums, target_sum, 0, lookup_table)
+
+# helper function
+def count_subset_sum_rec(nums, required_sum, current_index, lookup_table):
+    # base cases
+    if required_sum == 0:
+        return 1 
+    if current_index >= len(nums):
+        return 0    
+
+    # check if we have processed the same subproblem already or not
+    if lookup_table[current_index][required_sum] == -1:
+        # if the number at current_index does not exceed the 
+        # required_sum, we include it
+        sum1 = 0
+        if nums[current_index] <= required_sum:
+            sum1 = count_subset_sum_rec(nums, required_sum - nums[current_index], current_index + 1, lookup_table)
+
+        # recursive call, excluding the number at the current_index
+        sum2 = count_subset_sum_rec(nums, required_sum, current_index + 1, lookup_table)
+
+        lookup_table[current_index][required_sum] = sum1 + sum2
+
+    return lookup_table[current_index][required_sum]
+
+# -- Bottom up
+def count_subset_sum(nums, target_sum):
+    nums_len = len(nums)
+    # lookup table of size nums_len x (targetSum + 1)
+    lookup = [[0 for i in range(target_sum + 1)] for j in range(nums_len)]
+
+    # Base case 1
+    if nums[0] == 0:
+        lookup[0][0] = 2
+    # Base case 2
+    else: 
+        lookup[0][0] = 1
+        if nums[0] <= target_sum:
+            lookup[0][nums[0]] = 1
+
+    for current in range(1, nums_len):
+        for required_sum in range(0, target_sum + 1):
+            # included
+            sum1 = 0
+            if nums[current] <= required_sum:
+                sum1 = lookup[current - 1][required_sum - nums[current]]
+                
+            # excluded
+            sum2 = lookup[current - 1][required_sum]
+
+            lookup[current][required_sum] = sum1 + sum2
+    
+    return lookup[nums_len - 1][target_sum]
