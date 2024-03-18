fb663010c9e01f5d29c57e0f207af7869ee9e71b
minhtu-nguyen <minhtu-nguyen@outlook.com>
2024-03-18 11:04:38 +0700

    Add basic code for Python

---
+++ full-speed/Basics/python.py
@@ -0,0 +1,32 @@
+### Variables and Types
+## Numbers
+myint = 7
+myfloat = 7.0
+myfloat = float(7)
+## Strings
+mystring = 'hello'
+mystring = "hello"  
+mystring = "Don't worry about apostrophes" 
+# Simple operators can be executed on numbers and strings:
+one = 1
+two = 2
+three = one + two
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+# Assignments can be done "simultaneously" 
+a, b = 3, 4
+# Mixing operators between numbers and strings is not supported
+# Casting
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+# Get the Type
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+# Variable names are case-sensitive
+a = 4
+A = "Sally"
+#A will not overwrite a
